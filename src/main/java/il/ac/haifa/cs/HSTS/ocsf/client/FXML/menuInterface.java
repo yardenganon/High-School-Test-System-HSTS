33069997fd78405ef8ba8adaa593f87b04b1c195
Daniel Levy <levydaniel107@gmail.com>
2020-05-04 21:54:24 +0300

    client interfacw

--- src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/menuInterface.java
+++ src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/menuInterface.java
@@ -4,12 +4,45 @@
 
 package il.ac.haifa.cs.HSTS.ocsf.client.FXML;
 
+//import com.gluonhq.charm.glisten.control.Icon;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class menuInterface {
 
-    @FXML // fx:id="menu"
-    private AnchorPane menu; // Value injected by FXMLLoader
+    @FXML // fx:id="logoutbtn"
+    private Button logoutbtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="Showbtn"
+    private Button Showbtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tablev"
+    private TableView<?> tablev; // Value injected by FXMLLoader
+
+    @FXML // fx:id="searchTF"
+    private TextField searchTF; // Value injected by FXMLLoader
+
+    @FXML // fx:id="searchbtn"
+    private Button searchbtn; // Value injected by FXMLLoader
+
+    //@FXML // fx:id="searchicon"
+    //private Icon searchicon; // Value injected by FXMLLoader
+
+    @FXML
+    void Show(ActionEvent event) {
+    	searchbtn.setVisible(true);
+    	searchTF.setVisible(true);
+    	tablev.setVisible(true);
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
 
 }
