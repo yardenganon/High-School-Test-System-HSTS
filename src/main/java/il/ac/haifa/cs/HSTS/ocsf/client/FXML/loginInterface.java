03867d90df0a9bc87fe593898da239c1a188abb9
ohadf <ohadf@DESKTOP-PM3963M>
2020-05-04 19:51:53 +0300

    client interface

---
+++ src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/loginInterface.java
@@ -0,0 +1,38 @@
+package il.ac.haifa.cs.HSTS.ocsf.client.FXML;
+
+import il.ac.haifa.cs.HSTS.Command;
+import il.ac.haifa.cs.HSTS.HSTSClientInterface;
+import il.ac.haifa.cs.HSTS.ocsf.server.Entities.Question;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class loginInterface {
+	/**
+	 * Sample Skeleton for 'GUIInterface.fxml' Controller Class
+	 */
+
+    @FXML // fx:id="loginBtn"
+    private Button loginBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="usernameTF"
+    private TextField usernameTF; // Value injected by FXMLLoader
+
+    @FXML // fx:id="passwordTF"
+    private TextField passwordTF; // Value injected by FXMLLoader
+
+    @FXML
+    void login(ActionEvent event) {
+    	
+    }
+
+}
+
+
+
