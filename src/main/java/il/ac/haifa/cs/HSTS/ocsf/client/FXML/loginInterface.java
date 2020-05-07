6572c917193efdb708b988ac1d3bc1978416de1a
ohadf <ohad93@gmail.com>
2020-05-07 20:33:32 +0300

    Login interface

--- src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/loginInterface.java
+++ src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/loginInterface.java
@@ -5,16 +5,23 @@ import il.ac.haifa.cs.HSTS.HSTSClientInterface;
 import il.ac.haifa.cs.HSTS.ocsf.server.Entities.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class loginInterface {
 	/**
-	 * Sample Skeleton for 'GUIInterface.fxml' Controller Class
+	 * Sample Skeleton for 'loginInterface.fxml' Controller Class
 	 */
-	static private Command commandFromServer = null;
+	private static Command commandFromServer = null;
 
     @FXML // fx:id="loginBtn"
     private Button loginBtn; // Value injected by FXMLLoader
@@ -23,33 +30,52 @@ public class loginInterface {
     private TextField usernameTF; // Value injected by FXMLLoader
 
     @FXML // fx:id="passwordTF"
-    private TextField passwordTF; // Value injected by FXMLLoader
+    private PasswordField passwordTF; // Value injected by FXMLLoader
+
+    @FXML // fx:id="errorLB"
+    private Label errorLB; // Value injected by FXMLLoader
 
     @FXML
     void login(ActionEvent event) throws IOException{
         // Creating command here and send it to client
-        Command command = new Command("login","users",
-                usernameTF.getCharacters().toString(),
-                passwordTF.getCharacters().toString());
+        errorLB.setVisible(false);
+        String username = usernameTF.getCharacters().toString();
+        String password = passwordTF.getCharacters().toString();
+
+        if (username.isBlank() || password.isBlank()) {
+            errorLB.setVisible(true);
+            return;
+        }
+        Command command = new Command("login","users", username, password);
         HSTSClientInterface.sendCommandToServer(command);
 
         while (commandFromServer == null){
             System.out.print("");
         }
+
         System.out.println(commandFromServer);
         User userLoggedIn;
-        if (commandFromServer.getReturnedObject() != null) {
+        if (commandFromServer.getReturnedObject() == null) {
+            errorLB.setVisible(true);
+            commandFromServer = null;
+            return;
+        }
+        else {
             userLoggedIn = (User) commandFromServer.getReturnedObject();
             System.out.println("User: "+userLoggedIn.getUsername() + " is logged in");
-            MainClass.setRoot("menuInterface");
+            Scene scene = new Scene(loadFXML("menuInterface"));
+            Stage stage = (Stage) loginBtn.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Menu");
+            menuInterface.setUser(userLoggedIn);
+            ((Label) scene.lookup("#helloLB")).setText("Hello " + userLoggedIn.getFirst_name());
             commandFromServer = null;
         }
-        else
-        {
-            // Label up
-            System.out.println("sad");
-        }
+    }
 
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainClass.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void receivedCommandFromServer(Command command){
--- src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/menuInterface.java
+++ src/main/java/il/ac/haifa/cs/HSTS/ocsf/client/FXML/menuInterface.java
@@ -5,17 +5,35 @@
 package il.ac.haifa.cs.HSTS.ocsf.client.FXML;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 //import com.gluonhq.charm.glisten.control.Icon;
 
+import il.ac.haifa.cs.HSTS.ocsf.server.ConnectionToClient;
+import il.ac.haifa.cs.HSTS.ocsf.server.Entities.User;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class menuInterface {
+public class menuInterface{
+    /**
+     * Sample Skeleton for 'menuInterface.fxml' Controller Class
+     */
+
+    private static User user = null;
+
+    @FXML // fx:id="helloLB"
+    private Label helloLB; // Value injected by FXMLLoader
 
     @FXML // fx:id="logoutbtn"
     private Button logoutBtn; // Value injected by FXMLLoader
@@ -41,13 +59,52 @@ public class menuInterface {
     	searchTF.setVisible(true);
     	tablev.setVisible(true);
 
+        // Creating command here and send it to client
+       /* Command command = new Command("login","users",
+                usernameTF.getCharacters().toString(),
+                passwordTF.getCharacters().toString());
+        HSTSClientInterface.sendCommandToServer(command);
+
+        while (commandFromServer == null){
+            System.out.print("");
+        }
+        System.out.println(commandFromServer);
+        User userLoggedIn;
+        if (commandFromServer.getReturnedObject() != null) {
+            userLoggedIn = (User) commandFromServer.getReturnedObject();
+            System.out.println("User: "+userLoggedIn.getUsername() + " is logged in");
+            MainClass.setRoot("menuInterface");
+            commandFromServer = null;
+        }
+        else
+        {
+            // Label up
+            System.out.println("sad");
+        }*/
 
 
     }
     
     @FXML
     void logout(ActionEvent event) throws IOException{
-    	MainClass.setRoot("loginInterface");
+        Scene scene = new Scene(loadFXML("loginInterface"));
+        Stage stage = (Stage) logoutBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Login");
     }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainClass.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        menuInterface.user = user;
+    }
+
 
 }
