63e8655fd217948c6b467024adb783cd85529aac
Yoav Ben Haim <yoavbh16@gmail.com>
2020-06-09 21:38:16 +0300

    updating command name

--- Entities/src/main/java/il/ac/haifa/cs/HSTS/server/CommandInterface/ReadyTestExtendedFacadeReadByTeacherCommand.java
+++ Entities/src/main/java/il/ac/haifa/cs/HSTS/server/CommandInterface/ReadyTestExtendedFacadeReadByTeacherCommand.java
@@ -1,9 +1,9 @@
 package il.ac.haifa.cs.HSTS.server.CommandInterface;
 
-public class ReadyTestFacadeReadByTeacherCommand extends CommandInterface{
+public class ReadyTestExtendedFacadeReadByTeacherCommand extends CommandInterface{
     private int teacherId;
 
-    public ReadyTestFacadeReadByTeacherCommand(int teacherId){
+    public ReadyTestExtendedFacadeReadByTeacherCommand(int teacherId){
         this.teacherId = teacherId;
     }
 
