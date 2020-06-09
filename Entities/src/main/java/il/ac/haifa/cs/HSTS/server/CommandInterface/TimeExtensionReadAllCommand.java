63907d595776fcf7475e58f49d0ce55e5798badf
Yoav Ben Haim <yoavbh16@gmail.com>
2020-06-09 16:08:45 +0300

    adding TimeExtensionReadAllCommand upadating CommandRouter upadating TimeExtensionController updating TimeExtensionRepository

---
+++ Entities/src/main/java/il/ac/haifa/cs/HSTS/server/CommandInterface/TimeExtensionReadAllCommand.java
@@ -0,0 +1,11 @@
+package il.ac.haifa.cs.HSTS.server.CommandInterface;
+
+import il.ac.haifa.cs.HSTS.server.Entities.Principle;
+import il.ac.haifa.cs.HSTS.server.Entities.TimeExtensionRequest;
+
+import java.util.List;
+
+public class TimeExtensionReadAllCommand extends CommandInterface{
+    public TimeExtensionReadAllCommand() {
+    }
+}
